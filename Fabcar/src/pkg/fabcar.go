686f3587ae48deda8fb59922eaae1cde8d4e1d18
hieubap <ngohieu1811@gmail.com>
2020-09-10 14:23:41 +0700

    update

--- Fabcar/src/pkg/fabcar.go
+++ Fabcar/src/pkg/fabcar.go
@@ -4,17 +4,12 @@ import (
 "fmt"
 "encoding/json"
 "strconv"	
-
-"github.com/hyperledger/fabric-contract-api-go/contractapi"	
+"github.com/hyperledger/fabric-contract-api-go/contractapi"
+"packageCar"
+"os"
+//"github.com/hyperledger/fabric-contract-api-go/contractapi"
 )
-//type Contract struct{
-//	name string
-//	Info string 
-//	UnknowTransaction interface{}
-//	BeforeTransaction interface{}
-//	AfterTransaction interface{}
-//	TransactionContextHandler SettableTransactionContextInterface
-//}
+
 // *********************************************************
 type SmartContract struct {
         contractapi.Contract
@@ -22,16 +17,49 @@ type SmartContract struct {
 
 // Car describes basic details of what makes up a car
 type Car struct {
+		Key    string
         Make   string `json:"make"`
         Model  string `json:"model"`
         Colour string `json:"colour"`
         Owner  string `json:"owner"`
+        Status bool   `json:"status"`
 }
 
 // QueryResult structure used for handling result of query
 type QueryResult struct {
         Key    string `json:"Key"`
         Record *Car
+}
+
+func (s *SmartContract) AddCarOfPackage(ctx contractapi.TransactionContextInterface,p packageCar.PackageCar) error {
+	for _, car := range p.Cars {
+                carAsBytes, _ := json.Marshal(car)
+                err := ctx.GetStub().PutState(car.Key, carAsBytes)
+
+                if err != nil {
+                        return fmt.Errorf("Failed to put to world state. %s", err.Error())
+                }
+        }
+	return nil
+}
+
+func (s *SmartContract) Transport(ctx contractapi.TransactionContextInterface,p packageCar.PackageCar){
+	p.Transport()
+}
+
+func (s *SmartContract) HandOver(ctx contractapi.TransactionContextInterface,p packageCar.PackageCar,own string) error{
+	p.ChangePackageOwner(own)
+	
+	for _, car := range p.Cars {
+                carAsBytes, _ := json.Marshal(car)
+                err := ctx.GetStub().PutState(car.Key, carAsBytes)
+
+                if err != nil {
+                        return fmt.Errorf("Failed to put to world state. %s", err.Error())
+                }
+        }
+	
+	return nil
 }
 
 func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) error {
@@ -128,22 +156,65 @@ func (s *SmartContract) ChangeCarOwner(ctx contractapi.TransactionContextInterfa
                 return err
         }
 
-        car.Owner = newOwner
+        car.Owner = newOwner // thay đổi chủ sở hữu
 
-        carAsBytes, _ := json.Marshal(car)
+        carAsBytes, _ := json.Marshal(car) // chuyển dữ liệu sang dạng byte
 
-        return ctx.GetStub().PutState(carNumber, carAsBytes)
+        return ctx.GetStub().PutState(carNumber, carAsBytes) // cập nhật lại
 }
 
 func main() {
-        chaincode, err := contractapi.NewChaincode(new(SmartContract))
+//	p := packageCar.PackageCar{}
+//	p.InitPackage()
+//	p.ShowAll()
+	
+//	car := Car{
+//                Make:   "make",
+//                Model:  "model",
+//                Colour: "colour",
+//                Owner:  "owner",
+//        }
+//
+//    carAsBytes, _ := json.Marshal(car)
+//        
+//	transactioncontext := new(contractapi.TransactionContext)
+//	
+//	if carAsBytes == nil{
+//		fmt.Println("error")
+//	}
+//	if transactioncontext.GetStub() == nil{
+//		fmt.Println("error")
+//	}
+//	
+//	transactioncontext.GetStub().PutState("STATE",carAsBytes)
+//	
+//	carAsBytes,_ = transactioncontext.GetStub().GetState("STATE")
+//	car1 := new(Car)
+//	
+//	json.Unmarshal(carAsBytes,car1)
+//	car2 := *car1
+//	fmt.Println(car2)
+	
+//	s := SmartContract{}
+//	s.InitLedger(transactioncontext)
+	
+	
+//	chaincode, err := contractapi.NewChaincode(new(SmartContract))
+//
+//        if err != nil {
+//                fmt.Printf("Error create fabcar chaincode: %s", err.Error())
+//                return
+//        }
+//
+//        if err := chaincode.Start(); err != nil {
+//                fmt.Printf("Error starting fabcar chaincode: %s", err.Error())
+//        }
 
-        if err != nil {
-                fmt.Printf("Error create fabcar chaincode: %s", err.Error())
-                return
-        }
-
-        if err := chaincode.Start(); err != nil {
-                fmt.Printf("Error starting fabcar chaincode: %s", err.Error())
-        }
+//	os.Setenv("CORE_CHAINCODE_ID_NAME","E:/WorkspaceEclipse/Fabcar/src/Fabcar")
+//	if os.Getenv("CORE_CHAINCODE_ID_NAME") == ""{
+//		fmt.Println("null")
+//	}
+	fmt.Println(os.Getenv("CORE_CHAINCODE_ID_NAME"))
+	
+	
 }
--- packageCar/packagecar.go
+++ packageCar/packagecar.go
@@ -1,4 +1,4 @@
-package main
+package packageCar
 
 import (
 	"fmt"
@@ -19,23 +19,24 @@ type Car struct {
         Model  string `json:"model"`
         Colour string `json:"colour"`
         Owner  string `json:"owner"`
+        Status bool   `json:"status"`
 }
 
 func (s *PackageCar) InitPackage(){
         cars := []Car{
-                Car{Make: "Toyota", Model: "Prius", Colour: "blue", Owner: "Tomoko"},
-                Car{Make: "Ford", Model: "Mustang", Colour: "red", Owner: "Brad"},
-                Car{Make: "Hyundai", Model: "Tucson", Colour: "green", Owner: "Jin Soo"},
-                Car{Make: "Volkswagen", Model: "Passat", Colour: "yellow", Owner: "Max"},
-                Car{Make: "Tesla", Model: "S", Colour: "black", Owner: "Adriana"},
-                Car{Make: "Peugeot", Model: "205", Colour: "purple", Owner: "Michel"},
-                Car{Make: "Chery", Model: "S22L", Colour: "white", Owner: "Aarav"},
-                Car{Make: "Fiat", Model: "Punto", Colour: "violet", Owner: "Pari"},
-                Car{Make: "Tata", Model: "Nano", Colour: "indigo", Owner: "Valeria"},
-                Car{Make: "Holden", Model: "Barina", Colour: "brown", Owner: "Shotaro"},
+                Car{Make: "Toyota", Model: "Prius", Colour: "blue", Owner: "Tomoko",Status: false},
+                Car{Make: "Ford", Model: "Mustang", Colour: "red", Owner: "Brad",Status: false},
+                Car{Make: "Hyundai", Model: "Tucson", Colour: "green", Owner: "Jin Soo",Status: false},
+                Car{Make: "Volkswagen", Model: "Passat", Colour: "yellow", Owner: "Max",Status: false},
+                Car{Make: "Tesla", Model: "S", Colour: "black", Owner: "Adriana",Status: false},
+                Car{Make: "Peugeot", Model: "205", Colour: "purple", Owner: "Michel",Status: false},
+                Car{Make: "Chery", Model: "S22L", Colour: "white", Owner: "Aarav",Status: false},
+                Car{Make: "Fiat", Model: "Punto", Colour: "violet", Owner: "Pari",Status: false},
+                Car{Make: "Tata", Model: "Nano", Colour: "indigo", Owner: "Valeria",Status: false},
+                Car{Make: "Holden", Model: "Barina", Colour: "brown", Owner: "Shotaro",Status: false},
         }
         
-        s.Status = true
+        s.Status = false
         i := len(s.Cars)
 
         for _, car := range cars {
@@ -52,6 +53,7 @@ func (s *PackageCar) CreateCar(carNumber string, make string, model string, colo
                 Model:  model,
                 Colour: colour,
                 Owner:  owner,
+                Status: false,
         }
         s.Cars = append(s.Cars,car)       
 }
@@ -81,12 +83,19 @@ func (s *PackageCar) ChangeCarOwner(carNumber string, newOwner string) {
 }
 
 func (s *PackageCar) Transport(){
-	s.Status = false
+	s.Status = true
+	for i,_ := range s.Cars{
+	    	s.Cars[i].Status = true
+	    } 
 }
 
 func (s *PackageCar) DoneHandOver(name string){
-	s.Status = true
+	s.Status = false
 	s.ChangePackageOwner(name)
+	
+	for i,_ := range s.Cars{
+	    	s.Cars[i].Status = false
+	    }
 }
 
 func (s *PackageCar) ChangePackageOwner(name string){
@@ -111,46 +120,47 @@ func (s *PackageCar)ShowAll(){
 	}
 }
 
-func main(){
-	fmt.Println("   *************************************************  tao package car  ******************** \n")
-	fmt.Println("-----------------  init package car --------------------- \n")
-	
-	pkg := PackageCar{}
-	pkg.ShowAll()
-	
-	fmt.Println("-----------------  test AddCar --------------------------- \n")
-	car := &Car{Key: "MyID",Make: "No-Make", Model: "No-Model", Colour: "No-Color", Owner: "Ngo-Quang-Hieu"};
-	pkg.AddCar(*car)
-	pkg.ShowAll()
-	
-	fmt.Println("-----------------  test func InitPackage ----------------- \n")
-	pkg.InitPackage()
-	pkg.ShowAll()
-	
-	fmt.Println("-----------------  test func CreateCar ------------------- \n")
-	pkg.CreateCar("A","B","C","D","E")
-	pkg.ShowAll()
-	
-	fmt.Println("-----------------  test func QueryCar -------------------- \n")
-	car2 := pkg.QueryCar("MyID")
-	fmt.Println(*car2)
-	
-	fmt.Println("-----------------  test func QueryAllCar ----------------- \n")
-	allcar := pkg.QueryAllCars()
-	fmt.Println(allcar)
-	
-	fmt.Println("-----------------  test func ChangeCarOwner -------------- \n")
-	pkg.ChangeCarOwner("MyID","Do-Duy-Hung")
-	car3 := pkg.QueryCar("MyID")
-	fmt.Println(*car3)
-	
-	fmt.Println("\n\n   *************************************************  van chuyen package car  ******************** \n")
-	pkg.Transport()
-	fmt.Println(pkg.Status)
-	
-	fmt.Println("\n\n   *************************************************  ban giao package car  ******************** \n")
-	pkg.DoneHandOver("Nguyen Duc Thang")
-	pkg.ShowAll()
-	fmt.Printf("Status = %v",pkg.Status)
-	
-}
+//func main(){
+//	fmt.Println("   *************************************************  tao package car  ******************** \n")
+//	fmt.Println("-----------------  init package car --------------------- \n")
+//	
+//	pkg := PackageCar{}
+//	pkg.ShowAll()
+//	
+//	fmt.Println("-----------------  test AddCar --------------------------- \n")
+//	car := &Car{Key: "MyID",Make: "No-Make", Model: "No-Model", Colour: "No-Color", Owner: "Ngo-Quang-Hieu"};
+//	pkg.AddCar(*car)
+//	pkg.ShowAll()
+//	
+//	fmt.Println("-----------------  test func InitPackage ----------------- \n")
+//	pkg.InitPackage()
+//	pkg.ShowAll()
+//	
+//	fmt.Println("-----------------  test func CreateCar ------------------- \n")
+//	pkg.CreateCar("A","B","C","D","E")
+//	pkg.ShowAll()
+//	
+//	fmt.Println("-----------------  test func QueryCar -------------------- \n")
+//	car2 := pkg.QueryCar("MyID")
+//	fmt.Println(*car2)
+//	
+//	fmt.Println("-----------------  test func QueryAllCar ----------------- \n")
+//	allcar := pkg.QueryAllCars()
+//	fmt.Println(allcar)
+//	
+//	fmt.Println("-----------------  test func ChangeCarOwner -------------- \n")
+//	pkg.ChangeCarOwner("MyID","Do-Duy-Hung")
+//	car3 := pkg.QueryCar("MyID")
+//	fmt.Println(*car3)
+//	
+//	fmt.Println("\n\n   *************************************************  van chuyen package car  ******************** \n")
+//	pkg.Transport()
+//	pkg.ShowAll()
+//	fmt.Println(pkg.Status)
+//	
+//	fmt.Println("\n\n   *************************************************  ban giao package car  ******************** \n")
+//	pkg.DoneHandOver("Nguyen Duc Thang")
+//	pkg.ShowAll()
+//	fmt.Printf("Status = %v",pkg.Status)
+//	
+//}
